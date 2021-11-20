cef9eec762eb1f851962456d9813578da0acbc8c
Anita Angelidou <anitawebdesigns@gmail.com>
2021-11-20 21:48:43 +0200

    refactoring:  moved script to external functions.js file

---
+++ javascript/functions.js
@@ -0,0 +1,40 @@
+
+
+
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(document).scrollTop() > 1290) {
+      $(".nav-bar").css("background-color", "rgb(255, 160, 160)");
+      $(".logo-fanari3").css({ "top": "40px" });
+    }
+    else if ($(document).scrollTop() > 850) {
+      $(".nav-bar").css("background-color", "rgb(29, 20, 23, 1)");
+      $(".logo-fanari3").css({ "top": "40px" });
+    }
+    else if ($(document).scrollTop() > 350) {
+      $(".nav-bar").css("background-color", "rgba(47, 73, 158, 0.514)");
+      $(".logo-fanari3").css({ "top": "40px" });
+    }
+    else {
+      $(".nav-bar").css("background-color", "transparent");
+      $(".logo-fanari3").css({ "top": "0px" });
+    }
+  });
+});
+
+
+
+// $(document).ready(function () {
+//   $(window).scroll(function () {
+//     if ($(window).scrollTop() > $(window).height()) {
+//       $(".nav-bar").css({ "background-color": "rgb(231, 220, 209, 1)" });
+//       $(".logo-fanari3").css({ "top": "30px" });
+//     }
+//     else {
+//       $(".nav-bar").css({ "background-color": "transparent" });
+//       $(".logo-fanari3").css({ "top": "0px" });
+//     }
+
+//   })
+// })
