0fec801546fcb43239217d2b753d3e78295905e1
Anita Angelidou <anitawebdesigns@gmail.com>
2021-11-28 12:07:24 +0200

    created products.js, a script with a table of objects for the products

---
+++ javascript/products.js
@@ -0,0 +1,20 @@
+const categFanaria = [
+    {
+        name: "fanarakia 1",
+        imageURL: "https://cdn1.iconfinder.com/data/icons/iconae-extra-light/100/thin_B-88-512.png",
+        alt: "image of fanaraki 1",
+        description: "a"
+
+    },
+
+    {
+        name: "fanari fixed 1",
+        imageURL: "./resources/images/fanaria/fanari fixed 1.png",
+        alt: "image of fanari fixed 1",
+        description: "b"
+
+    },
+  
+]
+
+let index=0;
