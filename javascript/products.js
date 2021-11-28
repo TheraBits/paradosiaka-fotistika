2e0b72d05dc0ab4e79f83f06312ce2460e944288
Anita Angelidou <anitawebdesigns@gmail.com>
2021-11-28 12:22:59 +0200

    completed arrow categFanaria in products.js

--- javascript/products.js
+++ javascript/products.js
@@ -1,7 +1,7 @@
 const categFanaria = [
     {
         name: "fanarakia 1",
-        imageURL: "https://cdn1.iconfinder.com/data/icons/iconae-extra-light/100/thin_B-88-512.png",
+        imageURL: "./resources/images/fanaria/fanarakia 1.jpg",
         alt: "image of fanaraki 1",
         description: "a"
 
@@ -14,7 +14,154 @@ const categFanaria = [
         description: "b"
 
     },
+
+    {
+        name: "fanari fixed 2",
+        imageURL: "./resources/images/fanaria/fanari fixed 2.png",
+        alt: "image of fanari fixed 2",
+        description: "c"
+
+    },
+
+    {
+        name: "fanari fixed 3",
+        imageURL: "./resources/images/fanaria/fanari fixed 3.png",
+        alt: "image of fanari fixed 3",
+        description: "d"
+
+    },
+
+    {
+        name: "fanari fixed 4",
+        imageURL: "./resources/images/fanaria/fanari fixed 4.png",
+        alt: "image of fanari fixed 4",
+        description: "b"
+
+    },
+
+    {
+        name: "fanari hanging 1",
+        imageURL: "./resources/images/fanaria/fanari hanging 1.png",
+        alt: "image of fanari hanging 1",
+        description: "A"
+
+    },
+
+    {
+        name: "fanari hanging 2",
+        imageURL: "./resources/images/fanaria/fanari hanging 2.jpg",
+        alt: "image of fanari hanging 2",
+        description: "B"
+
+    },
   
+    {
+        name: "fanari hanging 3",
+        imageURL: "./resources/images/fanaria/fanari hanging 3.jpg",
+        alt: "image of fanari hanging 3",
+        description: "C"
+
+    },
+
+    {
+        name: "fanari hanging 4",
+        imageURL: "./resources/images/fanaria/fanari hanging 4.jpg",
+        alt: "image of fanari hanging 4",
+        description: "D"
+
+    },
+
+    {
+        name: "fanari standing 1",
+        imageURL: "./resources/images/fanaria/fanari standing 1.png",
+        alt: "image of fanari standing 1",
+        description: "ab"
+
+    },
+
+    {
+        name: "fanari standing 2",
+        imageURL: "./resources/images/fanaria/fanari standing 2.png",
+        alt: "image of fanari standing 2",
+        description: "cd"
+
+    },
+
+    {
+        name: "fanari standing 3",
+        imageURL: "./resources/images/fanaria/fanari standing 3.png",
+        alt: "image of fanari standing 3",
+        description: "ef"
+
+    },
+
+    {
+        name: "fanari standing 4",
+        imageURL: "./resources/images/fanaria/fanari standing 4.jpg",
+        alt: "image of fanari standing 4",
+        description: "gh"
+
+    },
+
+    {
+        name: "fanari standing 5",
+        imageURL: "./resources/images/fanaria/fanari standing 5.png",
+        alt: "image of fanari standing 5",
+        description: "ij"
+
+    },
+
+    {
+        name: "fanari standing 6",
+        imageURL: "./resources/images/fanaria/fanari standing 6.png",
+        alt: "image of fanari standing 6",
+        description: "kl"
+
+    },
+
+    {
+        name: "fanari standing 7",
+        imageURL: "./resources/images/fanaria/fanari standing 7.png",
+        alt: "image of fanari standing 7",
+        description: "mn"
+
+    },
+
+    {
+        name: "fanari standing 8",
+        imageURL: "./resources/images/fanaria/fanari standing 8.png",
+        alt: "image of fanari standing 8",
+        description: "op"
+
+    },
+
+    {
+        name: "liston 1",
+        imageURL: "./resources/images/fanaria/liston 1.jpg",
+        alt: "image of fanari liston 1",
+        description: "AB"
+
+    },
+
+    {
+        name: "liston 2",
+        imageURL: "./resources/images/fanaria/liston 2.jpg",
+        alt: "image of fanari liston 2",
+        description: "CD"
+
+    },
+
+    {
+        name: "liston 3",
+        imageURL: "./resources/images/fanaria/liston 3.jpg",
+        alt: "image of fanari liston 3",
+        description: "EF"
+
+    },
+
+
 ]
+
+
 
 let index=0;
