b1a7d99fcd3f828d1a8b096aba7a2fea35141d90
Anita Angelidou <anitawebdesigns@gmail.com>
2022-04-09 22:23:38 +0300

    optimized images for web + added hero images for mobile for krani and kolones.html.

--- javascript/products.js
+++ javascript/products.js
@@ -1,345 +1,296 @@
 const categFanaria = [
-    {
-        name: "fanarakia 1",
-        imageURL: "./resources/images/fanaria/fanarakia 1.jpg",
-        alt: "image of fanaraki 1",
-        description: "a",
+  {
+    name: "fanarakia 1",
+    imageURL: "./resources/images/fanaria/fanarakia 1.jpg",
+    alt: "image of fanaraki 1",
+    description: "a",
+  },
 
-    },
+  {
+    name: "fanari fixed 1",
+    imageURL: "./resources/images/fanaria/fanari fixed 1.png",
+    alt: "image of fanari fixed 1",
+    description: "b",
+  },
 
-    {
-        name: "fanari fixed 1",
-        imageURL: "./resources/images/fanaria/fanari fixed 1.png",
-        alt: "image of fanari fixed 1",
-        description: "b",
+  {
+    name: "fanari fixed 2",
+    imageURL: "./resources/images/fanaria/fanari fixed 2.png",
+    alt: "image of fanari fixed 2",
+    description: "c",
+  },
 
+  {
+    name: "fanari fixed 3",
+    imageURL: "./resources/images/fanaria/fanari fixed 3.png",
+    alt: "image of fanari fixed 3",
+    description: "d",
+  },
 
-    },
+  {
+    name: "fanari fixed 4",
+    imageURL: "./resources/images/fanaria/fanari fixed 4.png",
+    alt: "image of fanari fixed 4",
+    description: "b",
+  },
 
-    {
-        name: "fanari fixed 2",
-        imageURL: "./resources/images/fanaria/fanari fixed 2.png",
-        alt: "image of fanari fixed 2",
-        description: "c"
+  {
+    name: "fanari hanging 1",
+    imageURL: "./resources/images/fanaria/fanari hanging 1.png",
+    alt: "image of fanari hanging 1",
+    description: "A",
+  },
 
-    },
+  {
+    name: "fanari hanging 2",
+    imageURL: "./resources/images/fanaria/fanari hanging 2.jpg",
+    alt: "image of fanari hanging 2",
+    description: "B",
+  },
 
-    {
-        name: "fanari fixed 3",
-        imageURL: "./resources/images/fanaria/fanari fixed 3.png",
-        alt: "image of fanari fixed 3",
-        description: "d"
+  {
+    name: "fanari hanging 3",
+    imageURL: "./resources/images/fanaria/fanari hanging 3.jpg",
+    alt: "image of fanari hanging 3",
+    description: "C",
+  },
 
-    },
+  {
+    name: "fanari hanging 4",
+    imageURL: "./resources/images/fanaria/fanari hanging 4.jpg",
+    alt: "image of fanari hanging 4",
+    description: "D",
+  },
 
-    {
-        name: "fanari fixed 4",
-        imageURL: "./resources/images/fanaria/fanari fixed 4.png",
-        alt: "image of fanari fixed 4",
-        description: "b"
+  {
+    name: "fanari standing 1",
+    imageURL: "./resources/images/fanaria/fanari standing 1.png",
+    alt: "image of fanari standing 1",
+    description: "ab",
+  },
 
-    },
+  {
+    name: "fanari standing 2",
+    imageURL: "./resources/images/fanaria/fanari standing 2.png",
+    alt: "image of fanari standing 2",
+    description: "cd",
+  },
 
-    {
-        name: "fanari hanging 1",
-        imageURL: "./resources/images/fanaria/fanari hanging 1.png",
-        alt: "image of fanari hanging 1",
-        description: "A"
+  {
+    name: "fanari standing 3",
+    imageURL: "./resources/images/fanaria/fanari standing 3.png",
+    alt: "image of fanari standing 3",
+    description: "ef",
+  },
 
-    },
+  {
+    name: "fanari standing 4",
+    imageURL: "./resources/images/fanaria/fanari standing 4.jpg",
+    alt: "image of fanari standing 4",
+    description: "gh",
+  },
 
-    {
-        name: "fanari hanging 2",
-        imageURL: "./resources/images/fanaria/fanari hanging 2.jpg",
-        alt: "image of fanari hanging 2",
-        description: "B"
+  {
+    name: "fanari standing 5",
+    imageURL: "./resources/images/fanaria/fanari standing 5.png",
+    alt: "image of fanari standing 5",
+    description: "ij",
+  },
 
-    },
-  
-    {
-        name: "fanari hanging 3",
-        imageURL: "./resources/images/fanaria/fanari hanging 3.jpg",
-        alt: "image of fanari hanging 3",
-        description: "C"
+  {
+    name: "fanari standing 6",
+    imageURL: "./resources/images/fanaria/fanari standing 6.png",
+    alt: "image of fanari standing 6",
+    description: "kl",
+  },
 
-    },
+  {
+    name: "fanari standing 7",
+    imageURL: "./resources/images/fanaria/fanari standing 7.png",
+    alt: "image of fanari standing 7",
+    description: "mn",
+  },
 
-    {
-        name: "fanari hanging 4",
-        imageURL: "./resources/images/fanaria/fanari hanging 4.jpg",
-        alt: "image of fanari hanging 4",
-        description: "D"
+  {
+    name: "fanari standing 8",
+    imageURL: "./resources/images/fanaria/fanari standing 8.png",
+    alt: "image of fanari standing 8",
+    description: "op",
+  },
 
-    },
+  {
+    name: "liston 1",
+    imageURL: "./resources/images/fanaria/liston 1.jpg",
+    alt: "image of fanari liston 1",
+    description: "AB",
+  },
 
-    {
-        name: "fanari standing 1",
-        imageURL: "./resources/images/fanaria/fanari standing 1.png",
-        alt: "image of fanari standing 1",
-        description: "ab"
+  {
+    name: "liston 2",
+    imageURL: "./resources/images/fanaria/liston 2.jpg",
+    alt: "image of fanari liston 2",
+    description: "CD",
+  },
 
-    },
-
-    {
-        name: "fanari standing 2",
-        imageURL: "./resources/images/fanaria/fanari standing 2.png",
-        alt: "image of fanari standing 2",
-        description: "cd"
-
-    },
-
-    {
-        name: "fanari standing 3",
-        imageURL: "./resources/images/fanaria/fanari standing 3.png",
-        alt: "image of fanari standing 3",
-        description: "ef"
-
-    },
-
-    {
-        name: "fanari standing 4",
-        imageURL: "./resources/images/fanaria/fanari standing 4.jpg",
-        alt: "image of fanari standing 4",
-        description: "gh"
-
-    },
-
-    {
-        name: "fanari standing 5",
-        imageURL: "./resources/images/fanaria/fanari standing 5.png",
-        alt: "image of fanari standing 5",
-        description: "ij"
-
-    },
-
-    {
-        name: "fanari standing 6",
-        imageURL: "./resources/images/fanaria/fanari standing 6.png",
-        alt: "image of fanari standing 6",
-        description: "kl"
-
-    },
-
-    {
-        name: "fanari standing 7",
-        imageURL: "./resources/images/fanaria/fanari standing 7.png",
-        alt: "image of fanari standing 7",
-        description: "mn"
-
-    },
-
-    {
-        name: "fanari standing 8",
-        imageURL: "./resources/images/fanaria/fanari standing 8.png",
-        alt: "image of fanari standing 8",
-        description: "op"
-
-    },
-
-    {
-        name: "liston 1",
-        imageURL: "./resources/images/fanaria/liston 1.jpg",
-        alt: "image of fanari liston 1",
-        description: "AB"
-
-    },
-
-    {
-        name: "liston 2",
-        imageURL: "./resources/images/fanaria/liston 2.jpg",
-        alt: "image of fanari liston 2",
-        description: "CD"
-
-    },
-
-    {
-        name: "liston 3",
-        imageURL: "./resources/images/fanaria/liston 3.jpg",
-        alt: "image of fanari liston 3",
-        description: "EF"
-
-    },
-
-
-]
-
+  {
+    name: "liston 3",
+    imageURL: "./resources/images/fanaria/liston 3.jpg",
+    alt: "image of fanari liston 3",
+    description: "EF",
+  },
+];
 
 const categFotistika = [
-    {
-        name: "lamp hanging 1",
-        imageURL: "./resources/images/Lampes/lamp hanging 1.png",
-        alt: "image of lamp hanging 1",
-        description: "lh1"
+  {
+    name: "lamp hanging 1",
+    imageURL: "./resources/images/Lampes/lamp hanging 1.png",
+    alt: "image of lamp hanging 1",
+    description: "lh1",
+  },
 
-    },
+  {
+    name: "lamp hanging 2",
+    imageURL: "./resources/images/Lampes/lamp hanging 2.png",
+    alt: "image of lamp hanging 2",
+    description: "lh2",
+  },
 
-    {
-        name: "lamp hanging 2",
-        imageURL: "./resources/images/Lampes/lamp hanging 2.png",
-        alt: "image of lamp hanging 2",
-        description: "lh2"
+  {
+    name: "lamp 1",
+    imageURL: "./resources/images/Lampes/lamp 1.png",
+    alt: "image of lamp 1",
+    description: "l1",
+  },
 
-    },
+  {
+    name: "lamp 2",
+    imageURL: "./resources/images/Lampes/lamp 2.png",
+    alt: "image of lamp 2",
+    description: "l2",
+  },
 
-    {
-        name: "lamp 1",
-        imageURL: "./resources/images/Lampes/lamp 1.png",
-        alt: "image of lamp 1",
-        description: "l1"
+  {
+    name: "lamp 3",
+    imageURL: "./resources/images/Lampes/lamp 3.png",
+    alt: "image of lamp 3",
+    description: "l3",
+  },
 
-    },
-  
-    {
-        name: "lamp 2",
-        imageURL: "./resources/images/Lampes/lamp 2.png",
-        alt: "image of lamp 2",
-        description: "l2"
+  {
+    name: "lamp 4",
+    imageURL: "./resources/images/Lampes/lamp 4.png",
+    alt: "image of lamp 4",
+    description: "l4",
+  },
 
-    },
+  {
+    name: "lamp 5",
+    imageURL: "./resources/images/Lampes/lamp 5.png",
+    alt: "image of lamp 5",
+    description: "l5",
+  },
 
-    {
-        name: "lamp 3",
-        imageURL: "./resources/images/Lampes/lamp 3.png",
-        alt: "image of lamp 3",
-        description: "l3"
+  {
+    name: "lamp 6",
+    imageURL: "./resources/images/Lampes/lamp 6.png",
+    alt: "image of lamp 6",
+    description: "l6",
+  },
 
-    },
+  {
+    name: "kiropigio 1",
+    imageURL: "./resources/images/Lampes/kiropigio 1.png",
+    alt: "image of kiropigio 1",
+    description: "k1",
+  },
 
-    {
-        name: "lamp 4",
-        imageURL: "./resources/images/Lampes/lamp 4.png",
-        alt: "image of lamp 4",
-        description: "l4"
+  {
+    name: "kiropigio 2",
+    imageURL: "./resources/images/Lampes/kiropigio 2.png",
+    alt: "image of kiropigio 2",
+    description: "k2",
+  },
 
-    },
+  {
+    name: "polielaios 1",
+    imageURL: "./resources/images/Lampes/polielaios 1.png",
+    alt: "image of polielaios 1",
+    description: "p1",
+  },
 
-    {
-        name: "lamp 5",
-        imageURL: "./resources/images/Lampes/lamp 5.png",
-        alt: "image of lamp 5",
-        description: "l5"
+  {
+    name: "polielaios 2",
+    imageURL: "./resources/images/Lampes/polielaios 2.png",
+    alt: "image of polielaios 2",
+    description: "p2",
+  },
 
-    },
-
-    {
-        name: "lamp 6",
-        imageURL: "./resources/images/Lampes/lamp 6.png",
-        alt: "image of lamp 6",
-        description: "l6"
-
-    },
-
-    {
-        name: "kiropigio 1",
-        imageURL: "./resources/images/Lampes/kiropigio 1.png",
-        alt: "image of kiropigio 1",
-        description: "k1"
-
-    },
-
-    {
-        name: "kiropigio 2",
-        imageURL: "./resources/images/Lampes/kiropigio 2.png",
-        alt: "image of kiropigio 2",
-        description: "k2"
-
-    },
-
-    {
-        name: "polielaios 1",
-        imageURL: "./resources/images/Lampes/polielaios 1.png",
-        alt: "image of polielaios 1",
-        description: "p1"
-
-    },
-
-    {
-        name: "polielaios 2",
-        imageURL: "./resources/images/Lampes/polielaios 2.png",
-        alt: "image of polielaios 2",
-        description: "p2"
-
-    },
-
-    {
-        name: "polielaios 3",
-        imageURL: "./resources/images/Lampes/polielaios 3.jpg",
-        alt: "image of polielaios 3",
-        description: "p3"
-
-    },
-    
-
-]
+  {
+    name: "polielaios 3",
+    imageURL: "./resources/images/Lampes/polielaios 3.jpg",
+    alt: "image of polielaios 3",
+    description: "p3",
+  },
+];
 
 const categKolones = [
-    {
-        name: "kolona 1",
-        imageURL: "./resources/images/Kolones/kolona 1.png",
-        alt: "image of kolona 1",
-        description: "ΚΦ1"
+  {
+    name: "kolona 1",
+    imageURL: "./resources/images/Kolones/kolona 1.png",
+    alt: "image of kolona 1",
+    description: "ΚΦ1",
+  },
 
-    },
-
-    {
-        name: "kolona 2",
-        imageURL: "./resources/images/Kolones/kolona 2.png",
-        alt: "image of kolona 2",
-        description: "ΚΦ2"
-
-    },
-]
-
+  {
+    name: "kolona 2",
+    imageURL: "./resources/images/Kolones/kolona 2.png",
+    alt: "image of kolona 2",
+    description: "ΚΦ2",
+  },
+];
 
 const categKrani = [
-    {
-        name: "kranos 1",
-        imageURL: "./resources/images/Krani and Simata/kranos 1.png",
-        alt: "image of kranos 1",
-        description: "Κ&Σ1"
+  {
+    name: "kranos 1",
+    imageURL: "./resources/images/Krani and Simata/kranos 1.png",
+    alt: "image of kranos 1",
+    description: "Κ&Σ1",
+  },
 
-    },
+  {
+    name: "kranos 2",
+    imageURL: "./resources/images/Krani and Simata/kranos 2.png",
+    alt: "image of kranos 2",
+    description: "Κ&Σ2",
+  },
 
-    {
-        name: "kranos 2",
-        imageURL: "./resources/images/Krani and Simata/kranos 2.jpg",
-        alt: "image of kranos 2",
-        description: "Κ&Σ2"
+  {
+    name: "kranos 3",
+    imageURL: "./resources/images/Krani and Simata/kranos 3.jpg",
+    alt: "image of kranos 3",
+    description: "Κ&Σ3",
+  },
 
-    },
+  {
+    name: "kranos 4",
+    imageURL: "./resources/images/Krani and Simata/kranos 4.jpg",
+    alt: "image of kranos 4",
+    description: "Κ&Σ4",
+  },
 
-    {
-        name: "kranos 3",
-        imageURL: "./resources/images/Krani and Simata/kranos 3.jpg",
-        alt: "image of kranos 3",
-        description: "Κ&Σ3"
+  {
+    name: "sima 1",
+    imageURL: "./resources/images/Krani and Simata/sima 1.png",
+    alt: "image of sima 1",
+    description: "Κ&Σ α",
+  },
 
-    },
-
-    {
-        name: "kranos 4",
-        imageURL: "./resources/images/Krani and Simata/kranos 4.jpg",
-        alt: "image of kranos 4",
-        description: "Κ&Σ4"
-
-    },
-
-    {
-        name: "sima 1",
-        imageURL: "./resources/images/Krani and Simata/sima 1.png",
-        alt: "image of sima 1",
-        description: "Κ&Σ α"
-
-    },
-
-    {
-        name: "sima 2",
-        imageURL: "./resources/images/Krani and Simata/sima2.png",
-        alt: "image of sima 2",
-        description: "Κ&Σ β"
-
-    },
-]
-
+  {
+    name: "sima 2",
+    imageURL: "./resources/images/Krani and Simata/sima2.png",
+    alt: "image of sima 2",
+    description: "Κ&Σ β",
+  },
+];
 
 // let index=0;
