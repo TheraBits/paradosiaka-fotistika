04d907d8993e3bf3f3a08f31a49401dd03a07ca1
Anita Angelidou <anitawebdesigns@gmail.com>
2022-04-24 12:03:16 +0300

    call to action margins top

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,29 +1,20 @@
-
-
-
-
 $(document).ready(function () {
   $(window).scroll(function () {
     if ($(document).scrollTop() > 1290) {
       $(".nav-bar").css("background-color", "rgb(216, 150, 166)");
-      $(".logo-fanari3").css({ "top": "40px" });
-    }
-    else if ($(document).scrollTop() > 850) {
+      $(".logo-fanari3").css({ top: "40px" });
+    } else if ($(document).scrollTop() > 850) {
       $(".nav-bar").css("background-color", "rgb(29, 20, 23, 1)");
-      $(".logo-fanari3").css({ "top": "40px" });
-    }
-    else if ($(document).scrollTop() > 350) {
+      $(".logo-fanari3").css({ top: "40px" });
+    } else if ($(document).scrollTop() > 350) {
       $(".nav-bar").css("background-color", "rgba(47, 73, 158, 0.514)");
-      $(".logo-fanari3").css({ "top": "40px" });
-    }
-    else {
+      $(".logo-fanari3").css({ top: "40px" });
+    } else {
       $(".nav-bar").css("background-color", "transparent");
-      $(".logo-fanari3").css({ "top": "0px" });
+      $(".logo-fanari3").css({ top: "0px" });
     }
   });
 });
-
-
 
 // $(document).ready(function () {
 //   $(window).scroll(function () {
@@ -38,5 +29,3 @@ $(document).ready(function () {
 
 //   })
 // })
-
-
