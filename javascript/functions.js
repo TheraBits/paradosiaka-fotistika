75673a8a029a28cacb39e344e8f77d0c7c3ca598
Anita Angelidou <anitawebdesigns@gmail.com>
2024-06-23 10:01:23 +0300

    functionality to store lang on navigation

--- javascript/functions.js
+++ javascript/functions.js
@@ -18,34 +18,6 @@ $(document).ready(function () {
   });
 });
 
-const toggleButton = document.getElementById("langToggle");
-let currentLang = document.getElementById("langToggleButton").className;
-
-toggleButton.addEventListener("click", function () {
-  // Toggle the language
-  currentLang = currentLang === "gr" ? "en" : "gr";
-
-  // Update the menu items
-  const links = document.querySelectorAll(".dropdown-menu a");
-  links.forEach((link) => {
-    const text = link.getAttribute(`data-lang-${currentLang}`);
-    link.textContent = text;
-  });
-
-  // Change the text of the page
-  const elements = document.querySelectorAll(`[data-lang-${currentLang}]`);
-  elements.forEach((element) => {
-    // Toggle the visibility of the flags
-    document.querySelector(".gr").style.display =
-      currentLang === "en" ? "none" : "inline";
-    document.querySelector(".en").style.display =
-      currentLang === "gr" ? "none" : "inline";
-    const text = element.getAttribute(`data-lang-${currentLang}`);
-    element.textContent = text;
-  });
-  console.log("elements", elements);
-});
-
 // $(document).ready(function () {
 //   $(window).scroll(function () {
 //     if ($(window).scrollTop() > $(window).height()) {
---
+++ javascript/lang.js
@@ -0,0 +1,46 @@
+const toggleButton = document.getElementById("langToggle");
+let currentLang = localStorage.getItem("lang") || "gr"; // Default to English
+
+// Update UI based on current language
+updateLanguage(currentLang);
+
+function updateLanguage(lang) {
+  currentLang = lang;
+  localStorage.setItem("lang", lang);
+
+  // Update menu items
+  const links = document.querySelectorAll(".dropdown-menu a");
+  links.forEach((link) => {
+    const text = link.getAttribute(`data-lang-${lang}`);
+    link.textContent = text;
+  });
+
+  // Change the text of the page
+  const elements = document.querySelectorAll(`[data-lang-${lang}]`);
+  elements.forEach((element) => {
+    const text = element.getAttribute(`data-lang-${lang}`);
+    element.textContent = text;
+  });
+
+  // Update flag visibility
+  document.querySelector(".gr").style.display =
+    lang === "en" ? "none" : "inline";
+  document.querySelector(".en").style.display =
+    lang === "gr" ? "none" : "inline";
+}
+
+// Handle navigation link click
+document.querySelectorAll("nav a").forEach((link) => {
+  link.addEventListener("click", () => {
+    // Don't change language on menu items click
+    if (link !== toggleButton) {
+      updateLanguage(currentLang);
+    }
+  });
+});
+
+// Handle toggle button click
+toggleButton.addEventListener("click", function () {
+  currentLang = currentLang === "gr" ? "en" : "gr";
+  updateLanguage(currentLang);
+});
